039145c953896a9059491d424ca198f4a0134f99
王志成 <wangzhicheng@tongxinyiliao.com>
2017-05-22 13:23:51 +0800

    production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,11 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+    # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
+
+  # Generate digests for assets URLs.
+  config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
