0d2c60d35ea6519c1a223aa6ad4009cee71ffc11
王志成 <wangzhicheng@tongxinyiliao.com>
2017-06-30 15:38:35 +0800

    登录模块修改

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,38 +1,33 @@
-class SessionsController < Clearance::SessionsController
-    REMEMBER_TOKEN_COOKIE = 'remember_token'.freeze
+class SessionsController < ApplicationController
     layout false
     def show
     end
 
     def create
-        @dc_user = authenticate(params)
-        # 验证码校验
-        if !verify_rucaptcha?()
+        username = params[:sessions]["username"]
+        password = params[:sessions]["password"]
+        @user = Manager.where(username: username).first if username.present?
+        if !verify_rucaptcha?(@user)
             flash[:error] = "验证码错误"
             redirect_to "/sessions"
             return
         end
-        # 用户校验
-        if @dc_user.blank?
+        if @user.present? && @user.authenticate(password).present?
+            # unless @user.is_admin?
+            #     flash[:error] = "非管理员用户"
+            #     redirect_to "/sessions"
+            #     return
+            # end
+            if @user.disable
+                flash[:error] = "账号已禁用"
+                redirect_to "/sessions"
+                return
+            end
+            session[:user] = @user
+            redirect_to "/"
+        else
             flash[:error] = "用户名或密码输入错误"
             redirect_to "/sessions"
-            return
-        else
-          sign_in(@dc_user) do |status|
-            if status.success?
-                flash[:error] = nil
-                unless @dc_user.is_admin?
-                    flash[:error] = "非管理员用户"
-                    redirect_to "/sessions"
-                    return
-                end
-                session[:user] = @dc_user
-                redirect_to "/"
-            else
-              flash[:error] = nil
-              redirect_to '/sessions'
-            end
-          end
         end
     end
     
