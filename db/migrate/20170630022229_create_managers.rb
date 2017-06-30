d6ba95c61651c49b5570f94085d5e3e8f08e3a91
王志成 <wangzhicheng@tongxinyiliao.com>
2017-06-30 17:27:25 +0800

    修改index

--- db/migrate/20170630022229_create_managers.rb
+++ db/migrate/20170630022229_create_managers.rb
@@ -1,12 +1,15 @@
 class CreateManagers < ActiveRecord::Migration[5.0]
   def change
     create_table :managers do |t|
-      t.string :username
+      t.string :username :varchar(255)
       t.string :realname
-      t.string :phone
+      t.string :phone :varchar(128)
       t.integer :role
       t.timestamps
     end
-    add_index :managers, :username, unique: true, name: "x_m_usrnme"
+    
+    add_index :managers, :phone,unique: true, name: "idx_managers_phone"
+    add_index :managers, :username, unique: true, name: "idx_managers_usrnme"
+
   end
 end
